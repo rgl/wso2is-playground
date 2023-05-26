2f2164d29ed4171595e99855754ecfb545532d12
Rui Lopes <rgl@ruilopes.com>
2023-05-26 06:59:33 +0100

    also destroy the test and client docker compose profiles

--- destroy.sh
+++ destroy.sh
@@ -3,5 +3,7 @@ set -euo pipefail
 
 # destroy the existing environment.
 #docker compose run --workdir /host destroy
+docker compose --profile test down --volumes
+docker compose --profile client down --volumes
 docker compose down --volumes
 rm -f terraform.{log,tfstate,tfstate.backup} tfplan
